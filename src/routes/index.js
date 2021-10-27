1dc1325d4ad1d38a1f2cd1bba4df9f3dba1e4147
PhilipShinnn <kichulshin8@gmail.com>
2021-10-27 17:41:26 +0900

    Add : 초기세팅수정중

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
+router.use('/', reviewRouter);
+
 router.use('/ping', (req, res) => {
   console.log('pong');
   return res.status(200).json({ message: 'pong' });
